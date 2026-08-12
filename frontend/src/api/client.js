44f5d9cd6690551dd6db2456f8f776b043071d11
f <df>
2026-08-12 19:09:19 +0530

    Configure frontend for deployed backend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -10,7 +10,7 @@ const api = axios.create({
 
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "http://localhost:3000/api",
+    "https://kudos-ij18.onrender.com/api" ,
 
   withCredentials:
     true,
